a160cfd8cf52ee69a10f093f14e4e54cdea3587d
Patrick <biyagapatrick@gmail.com>
2026-02-22 19:46:54 +0100

    Overriding from the project’s templates directory

--- app/views.py
+++ app/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def welcome_page(request):
-    return HttpResponse("<h5>Welcome here</h5>")
+    books_qty = 100
+    name_of_store = "Store"
+    context = {
+        'books_qty': books_qty,
+        'name_of_store': name_of_store
+    }
+
+    return render(request, 'index.html',context=context)
